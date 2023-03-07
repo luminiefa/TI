93074b7dcf51fb7f587e806c1e0d34e61ab2cc37
luminiefa <ludovic.schoonjans@cgdc.be>
2023-03-08 00:09:21 +0300

    ajout ex combat

--- ludo/Q2/intro OO/main.py
+++ ludo/Q2/intro OO/main.py
@@ -1,5 +1,8 @@
 from Animal import *
 from Voiture import *
+
+from Personnage import *
+
 chien = Animal("médor", "labrador", 50, "02/03/2023")
 chat = Animal("catou", "siamois", 5, "01/03/2023")
 
@@ -10,3 +13,12 @@ merco = Voiture("john", 55555555, 3434343434343)
 audi = Voiture("Marie", 22222222, 11111111)
 
 print(merco.immatriculation)
+
+perso1 = Personnage("cac", "war", "orc", 100, 10, 2)
+perso2 = Personnage("caster", "mago", "humain", 50, 20, 1)
+
+perso1.tape(perso2)
+perso1.tape(perso2)
+perso1.tape(perso2)
+perso1.tape(perso2)
+print(perso2.point_vie)
