d065c61bb7989313d7e8d1265ca6f2b279a6e4e9
luminiefa <ludovic.schoonjans@cgdc.be>
2023-05-23 11:43:44 +0200

    Update log.py

--- ludo/Q2/Progra/Projets/log.py
+++ ludo/Q2/Progra/Projets/log.py
@@ -22,23 +22,25 @@ class Log:
         :return: str - Le nom du programme ou "Unknown" si le nom n'a pas pu être extrait
         """
         try:
-            # Trouver la position du premier crochet ouvrant '['
-            open_bracket_pos = self.text.index('[')
+            # Diviser la ligne de log en mots
+            words = self.text.split()
 
-            # Trouver la position du dernier espace avant le premier crochet ouvrant
-            space_pos = self.text.rfind(' ', 0, open_bracket_pos)
+            # Chercher le premier mot qui se termine par un crochet ouvrant '['
+            for word in words:
+                if word.endswith('['):
+                    # Enlever le crochet ouvrant de la fin du mot
+                    program = word.rstrip('[')
 
-            # Extraire le nom du programme en utilisant les positions trouvées
-            program = self.text[space_pos + 1:open_bracket_pos].strip()
+                    # Vérifier si le nom du programme est constitué
+                    # uniquement de caractères alphanumériques et de soulignements
+                    if program.isalnum() or '_' in program:
+                        return program
 
-            # Vérifier si le nom du programme est constitué
-            # uniquement de caractères alphanumériques et de soulignements
-            if program.isalnum() or '_' in program:
-                return program
-            else:
-                return "Unknown"
+            # Si aucun nom de programme n'a pu être extrait, retourner "Unknown"
+            return "Unknown"
         except ValueError:
             return "Unknown"
+
 
     def __str__(self):
         """
